0cac601ea46b03886be86300520941c10ab5eedb
10184577song_jichao <10184577song_jichao@cn.tre-inc.com>
2023-05-12 16:46:49 +0900

    test

--- packages/daifuku/lib/main.dart
+++ packages/daifuku/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:daifuku/configs/app_global.dart';
-import 'package:daifuku/widgets/custom_app.dart';
 import 'package:daifuku/route/router_manager.dart';
+import 'package:daifuku/widgets/custom_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
